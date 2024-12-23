814fe2c2c458a33e6d7aa83ffb576dce8351ee10
xjasz <novoselov.s@v2grp.ru>
2024-12-23 14:22:44 +0700

    Добавил Migration.cs

--- EcoState/Program.cs
+++ EcoState/Program.cs
@@ -4,6 +4,7 @@ using EcoState.Helpers;
 using EcoState.Interfaces;
 using EcoState.Services;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -51,4 +52,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+if (args.Contains("--migrate"))
+{
+    Log.Information("Запускается миграция");
+    app.RunMigration();
+}
+else
+{
+    app.Run();
+}
