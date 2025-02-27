48ab34cca99d97720297eaa7d8cedd528246b162
xjasz <novoselov.s@v2grp.ru>
2025-02-27 16:54:19 +0700

    Два теста добавил для контроллера погоды

--- EcoState.Test/WeatherControllerTest.cs
+++ EcoState.Test/WeatherControllerTest.cs
@@ -3,9 +3,7 @@ using AutoMapper;
 using EcoState.Context;
 using EcoState.Controllers;
 using EcoState.Domain;
-using EcoState.Extensions;
 using EcoState.Helpers;
-using EcoState.Interfaces;
 using EcoState.ViewModels.Weather;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
@@ -18,6 +16,7 @@ namespace EcoState.Test;
 public class WeatherControllerTest
 {
     private Mock<ApplicationDbContext> _dbContext = new();
+    private readonly Fixture _fixture = new();
     
     private List<Weather> _testData = new();
     
@@ -36,36 +35,98 @@ public class WeatherControllerTest
     }
     
     [Test]
-    public async Task Test()
+    public async Task GetWeather_WithWeatherGetModel_ShouldReturnCorrectListWeatherViewModel()
     {
         // Arrange
-        var _mapper = new Mock<IMapper>();
-        var _settings = new Mock<IOptions<WeatherSettings>>();
-        var _factory = new Mock<IHttpClientFactory>();
+        var mapper = new Mock<IMapper>();
+        var settings = new Mock<IOptions<WeatherSettings>>();
+        var factory = new Mock<IHttpClientFactory>();
 
         var dateTimeNow = DateTime.Now;
-        
         var weatherGetModel = new WeatherGetModel()
         {
             Date = dateTimeNow
         };
 
-        var weatherList = new List<Weather>() { new Weather() { Date = dateTimeNow, Temperature = 1 } };
-        SetDataToContext(weatherList.AsQueryable());
+        var testWeather = _fixture.Build<Weather>()
+            .With(x => x.Date, dateTimeNow)
+            .Create();
 
-        _mapper.Setup(x => x.Map<List<WeatherViewModel>>(It.IsAny<List<Weather>>()))
-            .Returns(new List<WeatherViewModel>() { new WeatherViewModel() { Temperature = 1, Date = weatherGetModel.Date}});
+        _testData = new List<Weather>() { testWeather };
+        SetDataToContext(_testData.AsQueryable());
+
+        var weatherViewModelList = _testData.Select(weather => new WeatherViewModel()
+            {
+                Date = weather.Date,
+                Temperature = weather.Temperature,
+                WindDirection = weather.WindDirection,
+                WindSpeed = weather.WindSpeed,
+                IconUrl = weather.IconUrl
+            })
+            .Where(x => x.Date == dateTimeNow)
+            .ToList();
+
+        mapper.Setup(x => x.Map<List<WeatherViewModel>>(It.IsAny<List<Weather>>()))
+            .Returns(weatherViewModelList);
         
-        var controller = new WeatherController(_dbContext.Object, _mapper.Object, _settings.Object, _factory.Object);
-
+        var controller = new WeatherController(_dbContext.Object, mapper.Object, settings.Object, factory.Object);
+        
         // Act
         var result = await controller.GetWeather(weatherGetModel) as OkObjectResult;
 
         // Assert
-        //result.Result.Should().Be("");
+        result!.Value.Should().Be(weatherViewModelList);
+        foreach (var weather in weatherViewModelList)
+        {
+            weather.Date.Should().Be(dateTimeNow);
+        }
+        
+        _dbContext.Verify(x => x.Weathers, Times.Once);
+    }
+    
+    [Test]
+    public async Task SaveWeather_WithWeatherSaveModel_ShouldCorrectSaveWeather()
+    {
+        // Arrange
+        var mapper = new Mock<IMapper>();
+        var settings = new Mock<IOptions<WeatherSettings>>();
+        var factory = new Mock<IHttpClientFactory>();
 
-        //result.Value.Should().Be("");
-        result.Value.Should().Be("");
+        var weatherSaveModel = _fixture.Create<WeatherSaveModel>();
+
+        var weather = new Weather()
+        {
+            Date = weatherSaveModel.Date,
+            Temperature = weatherSaveModel.Temperature,
+            WindDirection = weatherSaveModel.WindDirection,
+            WindSpeed = weatherSaveModel.WindSpeed,
+            IconUrl = weatherSaveModel.IconUrl
+        };
+
+        var weatherViewModel = new WeatherViewModel()
+        {
+            Date = weatherSaveModel.Date,
+            Temperature = weatherSaveModel.Temperature,
+            WindDirection = weatherSaveModel.WindDirection,
+            WindSpeed = weatherSaveModel.WindSpeed,
+            IconUrl = weatherSaveModel.IconUrl
+        };
+        
+        mapper.Setup(x => x.Map<Weather>(weatherSaveModel))
+            .Returns(weather);
+        mapper.Setup(x => x.Map<WeatherViewModel>(weather))
+            .Returns(weatherViewModel);
+        
+        var controller = new WeatherController(_dbContext.Object, mapper.Object, settings.Object, factory.Object);
+        
+        // Act
+        var result = await controller.SaveWeather(weatherSaveModel) as OkObjectResult;
+
+        // Assert
+        result!.Value.Should().Be(weatherViewModel);
+        
+        _dbContext.Verify(x => x.Weathers.Add(weather), Times.Once);
+        _dbContext.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
     }
     
     private void SetDataToContext(IQueryable<Weather> testData)
